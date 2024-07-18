40ce0d36c1e97db6ce2f277ae7bc5bf75fa3ee96
SajalDevnath <63283752+SajalDevnath@users.noreply.github.com>
2024-07-18 15:28:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-//your code here!
-
+document.addEventListener("DOMContentLoaded", () => {
+  const list = document.getElementById("infi-list");
+  const addListItems = (num) => {
+    for (let i = 0; i < num; i++) {
+      const listItem = document.createElement("li");
+      listItem.textContent = `List Item ${list.children.length + 1}`;
+      list.appendChild(listItem);
+    }
+  };
+  addListItems(10);
+  list.addEventListener("scroll", () => {
+    if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+      addListItems(2);
+    }
+  });
+});
